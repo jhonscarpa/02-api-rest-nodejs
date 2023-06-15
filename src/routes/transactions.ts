0e4a074216fc88430d6ece835d391cec3e0b3c4b
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-06-15 19:13:41 -0300

    config typescript table

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -18,6 +18,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
     await knex('transactions').insert({
       id: randomUUID(),
       title,
+
       amount: type === 'credit' ? amount : amount * -1,
     })
 
