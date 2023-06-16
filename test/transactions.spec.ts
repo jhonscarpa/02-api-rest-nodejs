41d94302316ec251636c2b8b3c432f284e75721c
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-06-16 18:59:40 -0300

    semantic test adjustes

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,21 +1,23 @@
-import { test, beforeAll, afterAll } from 'vitest'
+import { it, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async () => {
-  await app.ready()
-})
-afterAll(async () => {
-  await app.close()
-})
+describe('Transactions routes', async () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
 
-test('User can create a new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 })
